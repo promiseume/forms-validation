f2a0305f79db5f16613e16cd9b076a2ee0626e35
Promise Ume <ngoziume1@gmail.com>
2022-01-10 16:49:26 +0100

    form validation

---
+++ src/forms.js
@@ -0,0 +1,73 @@
+import react from "react";
+import { useState, useEffect }  from "react"
+
+export default function Form() {
+    const initialValue ={ email: '', password: '', confirmPassword: ''}
+    const[form, setForm] = useState(initialValue);
+    const[error, setError] = useState({});
+    const[isSubmit, setSubmit] = useState(false);
+
+    const handleChange=(e)=>{
+        const {name, value } =e.target;
+        setForm({...form, [name]: value});
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        setError(validate(form))
+        setSubmit(true);
+    }
+    useEffect(() =>{
+      if(Object.keys(error).length === 0 && isSubmit){
+
+      }
+    },[error])
+
+    const validate =(values)=>{
+      const error = {};
+      const regex= /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+      if(!values.email){
+          error.email = 'Email is required'
+      }
+      else if(!regex.test(values.email)){
+        error.email = 'This is not a valid email'
+      }
+      if(!values.password){
+        error.password = 'Password is required'
+    }
+   else if(values.password.length < 9){
+      error.password = 'Password must be greater than 9 character'
+    }
+    if(!values.confirmPassword){
+        error.confirmPassword = 'Confirm your password'
+    }
+    else if(form.password !== form.confirmPassword){
+        error.confirmPassword ='Password does not match'
+    }
+    return error;
+    }
+  return (
+    <div className="container">
+       <form onSubmit={handleSubmit}>
+       {Object.keys(error).length === 0 && isSubmit ? (<div className="success">Successful!!</div>):(<div className="invalid">Login</div>)}
+          <h1>Login Form</h1>
+          <div>
+              <p>{error.email}</p>
+              <label for='email'>Email</label>
+              <input type="email" name="email" placeholder="Enter Your Email"  value={form.email} onChange={handleChange}/>
+          </div>
+        <div>
+        <p>{error.password}</p>
+            <label for='Password'>Password</label>
+            <input type="password" name="password" placeholder="Enter Your Password" value={form.password} onChange={handleChange} />
+        </div>
+        <div>
+        <p>{error.confirmPassword}</p>
+            <label for='confirmPassword'>Confirm Password</label>
+            <input type="password" name="confirmPassword" placeholder="Confim Your Password" value={form.confirmPassword} onChange={handleChange} />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
