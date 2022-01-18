1e6fe697b7df3cc6a4ce9e3fa3cb1bddf1c6f814
Promise Ume <ngoziume1@gmail.com>
2022-01-18 14:54:15 +0100

    added chat section

--- src/forms.js
+++ src/forms.js
@@ -1,6 +1,6 @@
 import react from "react";
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Form() {
   const initialValue = { email: "", password: "", confirmPassword: "" };
